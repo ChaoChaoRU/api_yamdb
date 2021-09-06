f2e136c9ab37bf070b76574c48e8833b13a2be79
ChaoChaoRU <meshkoff.ant@yandex.ru>
2021-09-07 00:27:17 +0300

    Made structure and models

---
+++ api_yamdb/reviews/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from .validators import year_validator
+
+class Genre(models.Model):
+    name = models.CharField
+    slug = models.SlugField(unique=True)
+
+    class Meta:
+        ordering = ['-id', ]
+
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=256)
+    slug = models.SlugField(unique=True, max_length=50)
+
+    class Meta:
+        ordering=['-id',]
+
+    def __str__(self):
+        return self.name
+
+class Title(models.Model):
+    name = models.CharField(db_index=True)
+    year = models.IntegerField(blank=True, validators=[year_validator])
+    description = models.TextField
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, 
+                                 related_name='titles', blank=True, null=True)
+    genre = models.ManyToManyField(Genre, related_name='titles', blank=True)
+    
+    class Meta:
+        ordering=['-id',]
