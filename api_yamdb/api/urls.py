6adc1bd56179aa417ce4192795796d8a5c1d71fc
mihailcoc <78297274+mihailcoc@users.noreply.github.com>
2021-09-10 10:18:53 +0300

    My second commit

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,23 +1,37 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
-from .views import (CategoryViewSet, CommentViewSet, GenreViewSet,
-                    ReviewViewSet, TitleViewSet)
 from .views import GetUsersViewSet, GetPatchDeteleUserView
+from .views import GenreViewSet, CategoryViewSet
+from .views import GetCreateTitleViewSet, GetPatchDeleteTitleViewSet
+from .views import GetCreateReviewViewSet, GetPatchDeleteReviewViewSet
+from .views import GetCreateCommentViewSet, GetPatchDeleteCommentViewSet
+
 
 router_v1 = DefaultRouter()
-router_v1.register('categories', CategoryViewSet, basename='categories')
-router_v1.register('genres', GenreViewSet, basename='genres')
-router_v1.register('titles', TitleViewSet, basename='titles')
-router_v1.register(
-    r'titles/(?P<title_id>\d+)/reviews', ReviewViewSet, basename='review')
-router_v1.register(
-    r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments',
-    CommentViewSet, basename='comment')
 router_v1.register('users', GetUsersViewSet, basename='users')
 router_v1.register(
     r'^users/(?P<username>[\w.@+-]+)/$',
     GetPatchDeteleUserView, basename='user')
+router_v1.register('genres', GenreViewSet, basename='genres')
+router_v1.register('categories', CategoryViewSet, basename='categories')
+router_v1.register('titles', GetCreateTitleViewSet, basename='titles')
+router_v1.register(
+    r'titles/(?P<title_id>\d+)/', GetPatchDeleteTitleViewSet,
+    basename='titles_detail')
+router_v1.register(
+    r'titles/(?P<title_id>\d+)/review', GetCreateReviewViewSet,
+    basename='review')
+router_v1.register(
+    r'titles/(?P<title_id>\d+)/review/(?P<review_id>\d+)/',
+    GetPatchDeleteReviewViewSet, basename='review_detail')
+router_v1.register(
+    r'titles/(?P<title_id>\d+)/review/(?P<review_id>\d+)/com',
+    GetCreateCommentViewSet, basename='comment')
+router_v1.register(
+    r'titles/(?P<title_id>\d+)/review/(?P<review_id>\d+)/com/(?P<com_id>\d+)/',
+    GetPatchDeleteCommentViewSet, basename='comment_detail')
+
 
 urlpatterns = [
     path('v1/', include(router_v1.urls)),
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth import get_user_model
-from rest_framework import viewsets, mixins, filters
+from rest_framework import filters
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.pagination import PageNumberPagination
 from reviews.models import Genre, Category, Title
@@ -7,7 +7,6 @@ from rest_framework.mixins import CreateModelMixin, ListModelMixin
 from rest_framework.mixins import RetrieveModelMixin, UpdateModelMixin
 from rest_framework.mixins import DestroyModelMixin
 from rest_framework.viewsets import GenericViewSet
-from django.contrib.auth.mixins import LoginRequiredMixin
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.permissions import IsAdminUser
 
@@ -19,46 +18,48 @@ from .pagination import CustomPagination
 from reviews.models import CustomUser, Genre, Category, Title
 from reviews.models import Review, Comment
 from .permissions import AuthorOrReadOnly, ModeratorOrReadOnly
-from .permissions import SuperUserOrReadOnly
+from .permissions import AdminOrReadOnly, SuperUserOrReadOnly
 
 User = get_user_model()
 
 
-class GetUsersViewSet(ListModelMixin):
+class GetUsersViewSet(ListModelMixin, GenericViewSet):
     queryset = CustomUser.objects.all()
     serializer_class = GetUsersSerializer
     permission_classes = (IsAdminUser,)
     pagination_class = CustomPagination
 
 
-class CreateUserViewSet(CreateModelMixin):
+class CreateUserViewSet(CreateModelMixin, GenericViewSet):
     serializer_class = CreateUserSerializer
     permission_classes = (IsAdminUser,)
 
 
-class GetPatchDeteleUserViewSet(RetrieveModelMixin, UpdateModelMixin,
-                                DestroyModelMixin, GenericViewSet):
+class GetCreateViewSet(CreateModelMixin, ListModelMixin, GenericViewSet):
     pass
 
 
-class GetPatchDeteleUserView(LoginRequiredMixin, GetPatchDeteleUserViewSet):
+class GetPatchDeteleViewSet(RetrieveModelMixin, UpdateModelMixin,
+                            DestroyModelMixin, GenericViewSet):
+    pass
+
+
+class GetPatchDeteleUserView(GetPatchDeteleViewSet):
     serializer_class = GetPatchDeteleUserSerializer
     permission_classes = (IsAdminUser,)
     filter_backends = [filters.SearchFilter]
     search_fields = ['username', ]
-    login_url = 'api/v1/auth/token/'
-    redirect_field_name = 'redirect_to'
 
 
-class CustomViewSet(mixins.CreateModelMixin, mixins.DestroyModelMixin,
-                    mixins.ListModelMixin, viewsets.GenericViewSet):
+class CustomViewSet(CreateModelMixin, DestroyModelMixin,
+                    ListModelMixin, GenericViewSet):
     pass
 
 
 class GenreViewSet(CustomViewSet):
     queryset = Genre.objects.all()
     serializers_class = GenreSerializer
-    permission_classes = (IsAuthenticatedOrReadOnly, IsAdminUser,)
+    permission_classes = (AdminOrReadOnly,)
     pagination_class = PageNumberPagination
     filter_backends = [filters.SearchFilter]
     search_fields = ['name', ]
@@ -68,23 +69,37 @@ class GenreViewSet(CustomViewSet):
 class CategoryViewSet(CustomViewSet):
     queryset = Category.objects.all()
     serializers_class = CategorySerializer
-    permission_classes = (IsAuthenticatedOrReadOnly, IsAdminUser,)
+    permission_classes = (AdminOrReadOnly,)
     pagination_class = PageNumberPagination
     filter_backends = [filters.SearchFilter]
     search_fields = ['name', ]
     lookup_field = 'slug'
 
 
-class TitleViewSet(viewsets.ModelViewSet):
+class GetCreateTitleViewSet(GetCreateViewSet):
     queryset = Title.objects.all()
     serializers_class = TitleSerializer
-    permission_classes = (IsAuthenticatedOrReadOnly, IsAdminUser,)
+    permission_classes = (AdminOrReadOnly,)
     pagination_class = PageNumberPagination
     filter_backends = [DjangoFilterBackend]
     filterset_class = TitleFilter
 
 
-class ReviewViewSet(viewsets.ModelViewSet):
+class GetPatchDeleteTitleViewSet(GetPatchDeteleViewSet):
+    queryset = Title.objects.all()
+    serializers_class = TitleSerializer
+    permission_classes = (AdminOrReadOnly,)
+    pagination_class = PageNumberPagination
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = TitleFilter
+
+
+class GetCreateReviewViewSet(GetCreateViewSet):
+    queryset = Review.objects.all()
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+
+
+class GetPatchDeleteReviewViewSet(GetPatchDeteleViewSet):
     queryset = Review.objects.all()
 
     def get_permissions(self):
@@ -96,6 +111,18 @@ class ReviewViewSet(viewsets.ModelViewSet):
             return (IsAdminUser)
 
 
-class CommentViewSet(viewsets.ModelViewSet):
+class GetCreateCommentViewSet(GetCreateViewSet):
     queryset = Comment.objects.all()
-    permission_classes = (IsAuthenticatedOrReadOnly, IsAdminUser,)
+    permission_classes = (IsAuthenticatedOrReadOnly,)
+
+
+class GetPatchDeleteCommentViewSet(GetPatchDeteleViewSet):
+    queryset = Comment.objects.all()
+
+    def get_permissions(self):
+        if self.user.is_superuser is True:
+            return (SuperUserOrReadOnly(),)
+        elif self.user.is_staff is False:
+            return (AuthorOrReadOnly(), ModeratorOrReadOnly())
+        else:
+            return (IsAdminUser)
