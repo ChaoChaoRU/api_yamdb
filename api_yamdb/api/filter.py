7bbc945a5dde34b4a611eb4e87ebeaa36800c003
ChaoChaoRU <meshkoff.ant@yandex.ru>
2021-09-15 14:25:58 +0300

    ready to base

--- api_yamdb/api/filter.py
+++ api_yamdb/api/filter.py
@@ -4,7 +4,6 @@ from reviews.models import Title
 
 
 class TitleFilter(rest.FilterSet):
-<<<<<<< HEAD
     name = rest.CharFilter(field_name='name', lookup_expr='contains')
     category = rest.CharFilter(
         field_name='category__slug',
@@ -13,19 +12,6 @@ class TitleFilter(rest.FilterSet):
     genre = rest.CharFilter(
         field_name='genre__slug',
         lookup_expr='contains'
-=======
-    name = rest.CharFilter(
-        field_name='name',
-        lookup_expr='contains'
-    ),
-    genre = rest.CharFilter(
-        field_name='genre__slug',
-        lookup_expr='contains'
-    ),
-    category = rest.CharFilter(
-        field_name='category__slug',
-        lookup_expr='contains'
->>>>>>> c9ee626fe0fe94b5419ff6ded6d5fd72240a0f67
     )
 
     class Meta:
