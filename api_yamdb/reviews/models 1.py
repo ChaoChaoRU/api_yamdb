193d6bb887a15122fd172ce62aee22763e38adbc
mihailcoc <78297274+mihailcoc@users.noreply.github.com>
2021-09-15 10:59:50 +0300

    My first commit

--- api_yamdb/reviews/models 1.py
+++ api_yamdb/reviews/models 1.py
@@ -1,45 +1,11 @@
+from django.contrib.auth.models import AbstractUser
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
-from django.db import transaction
-from django.utils import timezone
 
 from .validators import year_validator
 
-from django.contrib.auth.models import (
-    AbstractBaseUser, PermissionsMixin, BaseUserManager
-)
 
-
-class UserManager(BaseUserManager):
-
-    def _create_user(self, email, password, **extra_fields):
-        """
-        Creates and saves a User with the given email,and password.
-        """
-        if not email:
-            raise ValueError('The given email must be set')
-        try:
-            with transaction.atomic():
-                user = self.model(email=email, **extra_fields)
-                user.set_password(password)
-                user.save(using=self._db)
-                return user
-        except CustomUser.DoesNotExist:
-            raise ValueError('The given user must be set')
-
-    def create_user(self, email, password=None, **extra_fields):
-        extra_fields.setdefault('is_staff', False)
-        extra_fields.setdefault('is_superuser', False)
-        return self._create_user(email, password, **extra_fields)
-
-    def create_superuser(self, email, password, **extra_fields):
-        extra_fields.setdefault('is_staff', True)
-        extra_fields.setdefault('is_superuser', True)
-
-        return self._create_user(email, password=password, **extra_fields)
-
-
-class CustomUser(AbstractBaseUser, PermissionsMixin):
+class CustomUser(AbstractUser):
     class UserRole:
         USER = 'user'
         MODERATOR = 'moderator'
@@ -86,20 +52,6 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
         choices=UserRole.CHOICES,
         default=UserRole.USER,
     )
-    is_active = models.BooleanField(default=True)
-
-    is_staff = models.BooleanField(default=False)
-
-    date_joined = models.DateTimeField(default=timezone.now)
-
-    objects = UserManager()
-
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['first_name', 'last_name']
-
-    def save(self, *args, **kwargs):
-        super(CustomUser, self).save(*args, **kwargs)
-        return self
 
     @property
     def is_admin(self):
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,11 +1,45 @@
-from django.contrib.auth.models import AbstractUser
 from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
+from django.db import transaction
+from django.utils import timezone
 
 from .validators import year_validator
 
+from django.contrib.auth.models import (
+    AbstractBaseUser, PermissionsMixin, BaseUserManager
+)
 
-class CustomUser(AbstractUser):
+
+class UserManager(BaseUserManager):
+
+    def _create_user(self, email, password, **extra_fields):
+        """
+        Creates and saves a User with the given email,and password.
+        """
+        if not email:
+            raise ValueError('The given email must be set')
+        try:
+            with transaction.atomic():
+                user = self.model(email=email, **extra_fields)
+                user.set_password(password)
+                user.save(using=self._db)
+                return user
+        except CustomUser.DoesNotExist:
+            raise ValueError('The given user must be set')
+
+    def create_user(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        return self._create_user(email, password=password, **extra_fields)
+
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
     class UserRole:
         USER = 'user'
         MODERATOR = 'moderator'
@@ -15,11 +49,7 @@ class CustomUser(AbstractUser):
             (MODERATOR, 'moderator'),
             (ADMIN, 'admin'),
         ]
-#    CHOICES = (
-#        user,
-#        moderator,
-#        admin,
-#    )
+
     username = models.CharField(
         max_length=254,
         verbose_name='Имя пользователя',
@@ -56,6 +86,20 @@ class CustomUser(AbstractUser):
         choices=UserRole.CHOICES,
         default=UserRole.USER,
     )
+    is_active = models.BooleanField(default=True)
+
+    is_staff = models.BooleanField(default=False)
+
+    date_joined = models.DateTimeField(default=timezone.now)
+
+    objects = UserManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+
+    def save(self, *args, **kwargs):
+        super(CustomUser, self).save(*args, **kwargs)
+        return self
 
     @property
     def is_admin(self):
